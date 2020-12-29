a80b5bf6de7b64504dc294dc70ea2ec05462d774
Anton <antob.chebanitsa@gmail.com>
2020-12-29 14:58:36 +0300

    Working second

--- SecondGardener.cs
+++ SecondGardener.cs
@@ -6,38 +6,26 @@ namespace Homework.ITAcademy4
     {
         public void Handle()
         {
-            //var upperBound = GardenCreator.Garden.GetUpperBound(0) + 1;
-
-            //for (var i = 0; i < GardenCreator.Garden.Length; i++)
-            //{
-            //    if (i % upperBound == 0)
-            //    {
-            //        Console.WriteLine();
-            //    }
-
-            //    Console.Write("1");
-            //}
-
-            bool reverse = false;
-            for (int i = GardenCreator.Garden.GetLength(1); i == 0; i--)
+            var obverse = true;
+            for (var i = GardenCreator.Garden.GetLength(1); i == 0; i--)
             {
-                if (reverse)
+                if (obverse)
                 {
-                    for (int j = GardenCreator.Garden.GetLength(0) - 1; j >= 0; j--)
+                    for (var j = GardenCreator.Garden.GetLength(0) - 1; j >= 0; j--)
                     {
-                        Console.Write(GardenCreator.Garden[j, i] + " ");
+                        GardenCreator.Garden[i, j] = 1;
                     }
                 }
 
                 else
                 {
-                    for (int j = 0; j < GardenCreator.Garden.GetLength(0); j++)
+                    for (var j = 0; j < GardenCreator.Garden.GetLength(0); j++)
                     {
-                        Console.Write(GardenCreator.Garden[j, i] + " ");
+                        GardenCreator.Garden[i, j] = 1;
                     }
                 }
 
-                reverse = !reverse;
+                obverse = !obverse;
             }
         }
     }
